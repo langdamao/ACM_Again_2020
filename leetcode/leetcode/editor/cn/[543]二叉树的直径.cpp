b5ddd97f9dbd29ecda7a6aabb7ae9207be02998c
langdamao <lang--lang--lang@163.com>
2022-04-28 20:19:32 +0800

    add :"leetcode/leetcode/editor/cn/[104]344272214345217211346240221347232204346234200345244247346267261345272246.cpp":"leetcode/leetcode/editor/cn/[116]345241253345205205346257217344270252350212202347202271347232204344270213344270200344270252345217263344276247350212202347202271346214207351222210.cpp":"leetcode/leetcode/editor/cn/[144]344272214345217211346240221347232204345211215345272217351201215345216206.cpp":"leetcode/leetcode/editor/cn/[226]347277273350275254344272214345217211346240221.cpp":"leetcode/leetcode/editor/cn/[543]344272214345217211346240221347232204347233264345276204.cpp"

---
+++ leetcode/leetcode/editor/cn/[543]二叉树的直径.cpp
@@ -0,0 +1,50 @@
+//给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。 
+//
+// 
+//
+// 示例 : 
+//给定二叉树 
+//
+//           1
+//         / \
+//        2   3
+//       / \     
+//      4   5    
+// 
+//
+// 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。 
+//
+// 
+//
+// 注意：两结点之间的路径长度是以它们之间边的数目表示。 
+// Related Topics 树 深度优先搜索 二叉树 👍 1010 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int dfs(TreeNode* root){
+        if (!root) return 0;
+        int ld = dfs(root->left);
+        int rd = dfs(root->right);
+        ans = max(ld+rd+1,ans);
+        return max(ld,rd)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        dfs(root);
+        return max(ans-1,0);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
